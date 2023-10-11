f7a94da4d0b2add713b1040749e85c5f1683d6a7
Nando <Nando.cajic@skole.hr>
2023-10-11 11:00:06 +0200

    Nando

--- dnevnik/edit_save_dnevnik_rada.php
+++ dnevnik/edit_save_dnevnik_rada.php
@@ -11,17 +11,17 @@
 <h2>Promjena dnevnika rada</h2>
 
 <?php
-    mysql_connect("localhost", "root", "");
-    mysql_select_db("dnevnik_rada_psiholog");
-    $result = mysql_query("UPDATE dnevnik_rada SET opis='$_POST[opis_dnevnik_rada]' where id_dr='$_POST[id_dr]'");
+    mysqli_connect("localhost", "root", "");
+    mysqli_select_db("dnevnik_rada_psiholog");
+    $result = mysqli_query("UPDATE dnevnik_rada SET opis='$_POST[opis_dnevnik_rada]' where id_dr='$_POST[id_dr]'");
 
-if (mysql_affected_rows() == 1) 
+if (mysqli_affected_rows() == 1) 
 { 
 	echo "Promijene unesene.";
 }
 else
 {
-	echo "Niste ništa mijenjali.";
+	echo "Niste niÅ¡ta mijenjali.";
 }
 
 ?>
