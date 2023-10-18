5adc94e79e823d55465c1249c904b7e11a47ee83
gabrielk <kokangabriel2005@gmail.com>
2023-10-18 18:33:12 +0200

    kokan css fix

--- ucenik/dodaj_dosje.php
+++ ucenik/dodaj_dosje.php
@@ -4,7 +4,7 @@
     <title>Dodaj učenika</title>
     <meta http-equiv="Content-Type" content="text/html" />
     <meta charset="utf-8">
-    <link href="admin_css.css" rel="stylesheet" type="text/css" />
+    <link href="../admin_css.css" rel="stylesheet" type="text/css" />
 </head>
 <body>
 <div class="sve">
