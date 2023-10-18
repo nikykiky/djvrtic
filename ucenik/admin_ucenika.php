56b61acb217fcefba4b1c9aa0631de57da3ed6fa
gabrielk <kokangabriel2005@gmail.com>
2023-10-18 18:24:59 +0200

    kokan komit poporavak svega

--- ucenik/admin_ucenika.php
+++ ucenik/admin_ucenika.php
@@ -63,57 +63,6 @@ if (isset($_GET['razred'])) {
 }
  
 mysqli_close($con);
-	$con = mysqli_connect("localhost", "root", "", "dnevnik_rada_psiholog");
-	
-	$razredi = "SELECT oznaka_raz FROM razred";
-	$sql = mysqli_query($con,$razredi);
-	echo "
-	<form action='".$_SERVER['PHP_SELF']."' method='GET'>
-		<select name='razred'>
-		<option value='--'>--</option>";
-		while($raz = mysqli_fetch_array($razredi))
-		{
-			echo "<option value='".$raz['oznaka_raz']."'>".$raz['oznaka_raz']."</option>";
-		}
-		echo "</select>
-		<input type='submit' value='Pregledaj'/>
-	</form>";
-	
-	
-	if (isset($_POST))
-	{
-
-		$rezultat = mysqli_query($con,"
-		SELECT *
-		FROM ucenik_razred
-		INNER JOIN ucenik ON ucenik_razred.id_uc = ucenik.id_uc 
-		INNER JOIN razred ON ucenik_razred.id_ra = razred.id_raz 
-		WHERE razred.oznaka_raz = '$_GET[razred]'
-		order by oznaka_raz desc;");
-    
-		echo "<table border='1'>
-			<tr valign='top'>
-			<td><b>Ime</b></td>
-			<td><b>Prezime</b></td>
-			<td><b>Razred</b></td>
-			<td><b>Pregled</b></td>
-			</tr>";
-		
-		while($redak = mysqli_fetch_array($rezultat))
-		{
-		  $id = $redak['id_uc'];
-		  echo "<tr valign='top'><td>";
-		  echo $redak['ime'];
-		  echo "</td><td>";
-		  echo $redak['prezime'];
-		  echo "</td><td>";
-		  echo $redak['oznaka_raz'];
-		  echo "</td><td>";
-		  echo "<a href='pregled_ucenika.php?id_ucenika=$id'>Pregled</a>";
-		  echo "</td></tr>";
-		  }
-		echo "</table>";
-	}
 ?>
 </div>
 </body>
